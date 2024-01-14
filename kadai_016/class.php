04f45b7d3b3fd8c8a4f39936a9f8d52339734fb8
yutaro0525 <148600619+yutaro0525@users.noreply.github.com>
2024-01-14 12:53:22 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,51 @@
+<?php
+class Food {
+  //  プロパティを定義
+  private $name;
+  private $price;
+
+  public function show_price(int $price) {
+    $this->price = $price;
+    echo $this->price . '<br>';
+  }
+
+  public function __construct(string $name, int $price){
+      $this->name = $name;
+      $this->price = $price;
+    }
+}
+
+class Animal {
+  // プロパティを定義
+  private $name;
+  private $height;
+  private $weight;
+
+  public function show_height(int $height) {
+    $this->height = $height;
+    echo $this->height . '<br>';
+  }
+
+  public function __construct(string $name, int $height, int $weight){
+      $this->name = $name;
+      $this->height = $height;
+      $this->weight = $weight;
+}
+}
+
+// インスタンス化
+$potato = new Food('potato', 250);
+$dog = new Animal('dog', 60, 5000);
+
+// 各プロパティを出力
+print_r($potato);
+echo '<br>';
+print_r($dog);
+echo '<br>';
+
+// メソッドにアクセスし、値を代入する
+$potato->show_price(250);
+$dog->show_height(60);
+
+?>
+
